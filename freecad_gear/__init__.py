dba082067e650d87b359d550b10cb054d1297846
looooo <sppedflyer@gmail.com>
2015-06-26 14:47:56 +0200

    unused imports

--- freecad_gear/__init__.py
+++ freecad_gear/__init__.py
@@ -5,3 +5,5 @@ from freecad_gear.gearfunc._cycloide_tooth import cycloide_tooth
 from freecad_gear.gearfunc._bevel_tooth import bevel_tooth
 
 import freecad_gear.freecad
+
+__all__ = [involute_tooth, involute_rack, cycloide_tooth, bevel_tooth]
